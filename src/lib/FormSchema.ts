5df6db1a660c4b08ba5bd15606c79a5e5b1ef85f
emilianoagustin <pintos.emiliano@gmail.com>
2025-11-24 23:18:13 -0300

    changed formschema

--- src/lib/FormSchema.ts
+++ src/lib/FormSchema.ts
@@ -3,12 +3,12 @@ import { z } from "zod";
 export const formSchema = z.object({
   name: z
     .string()
-    .min(1, { error: "Name is required." })
-    .min(3, { error: "Product name must be at least 3 characters." })
-    .max(15, { error: "Max name length reached" }),
+    .min(1, "Name is required.")
+    .min(3, "Product name must be at least 3 characters.")
+    .max(15, "Max name length reached"),
   price: z.coerce.number().positive("Price must be a positive number"),
   categories: z
     .array(z.string())
-    .min(1, { error: "Category is required" })
-    .max(3, { error: "Only three or less categories allowed" }),
+    .min(1, "Category is required")
+    .max(3, "Only three or less categories allowed"),
 });
