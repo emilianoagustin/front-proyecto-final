904d43f652f1ced9da71fe74b67721e932989137
emilianoagustin <pintos.emiliano@gmail.com>
2025-11-24 23:12:32 -0300

    changed product form

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,20 +1,10 @@
-import { columns } from "@/components/Table/columns";
-import { DataTable } from "@/components/Table/data-table";
-import { Product } from "@/types/Product.types";
+import { ProductTable } from "@/components/Table/product-table";
 
-async function getData(): Promise<Product[]> {
-  const response = await fetch("http://localhost:8080/api/products");
-  const data = await response.json();
-
-  return data;
-}
-
-export default async function Admin() {
-  const data = await getData();
-
+export default async function AdminPage() {
   return (
-    <div className="container mx-auto py-10">
-      <DataTable columns={columns} data={data} />
+    <div className="flex flex-col gap-4 max-w-7xl mx-auto p-4 md:p-24">
+      <h1 className="text-2xl font-bold">Products</h1>
+      <ProductTable />
     </div>
   );
 }
---
+++ src/components/Form/product-form.tsx
@@ -0,0 +1,99 @@
+"use client";
+
+import { formSchema } from "@/lib/FormSchema";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { toast } from "sonner";
+import { useProducts } from "@/hooks/useProduct";
+import { CategoryInput } from "@/components/CategoryInput/category-input";
+import { Loader2 } from "lucide-react";
+import { IFormProps } from "@/types/Product.types";
+
+export function ProductForm({ product }: IFormProps) {
+  const { createProduct, isCreating, updateProduct, isUpdating } =
+    useProducts();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: product?.name || "",
+      price: product?.price || 0,
+      categories: product?.categories || [],
+    },
+  });
+  function onSubmit({ ...values }: z.infer<typeof formSchema>) {
+    if (product) {
+      updateProduct({ id: product.id, data: values });
+      toast.success("Product updated successfully!");
+    } else {
+      createProduct(values);
+      form.reset();
+      toast.success("Product created successfully!");
+    }
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <FormField
+          control={form.control}
+          name="name"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Product name</FormLabel>
+              <FormControl>
+                <Input placeholder="Running shoes" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="price"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Price</FormLabel>
+              <FormControl>
+                <Input type="number" placeholder="100" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="categories"
+          render={({ field, fieldState }) => (
+            <CategoryInput
+              value={field.value}
+              onChange={field.onChange}
+              error={fieldState.error?.message}
+            />
+          )}
+        />
+        <Button
+          type="submit"
+          disabled={isCreating || isUpdating}
+          className="h-9 w-18"
+        >
+          {((isCreating || isUpdating) && (
+            <Loader2 className="size-4 animate-spin" />
+          )) ||
+            (product ? "Update" : "Create")}
+        </Button>
+      </form>
+    </Form>
+  );
+}
